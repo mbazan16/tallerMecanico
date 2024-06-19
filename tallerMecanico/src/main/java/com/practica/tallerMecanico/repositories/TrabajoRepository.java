e9ddd80e7d0e756b203802aa438fb655e1d63fd6
fazambrano24 <fazambrano24@gmail.com>
2024-06-19 12:38:01 +0200

    Guardar

--- tallerMecanico/src/main/java/com/practica/tallerMecanico/repositories/TrabajoRepository.java
+++ tallerMecanico/src/main/java/com/practica/tallerMecanico/repositories/TrabajoRepository.java
@@ -5,9 +5,9 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.practica.tallerMecanico.entities.Trabajo;
 
-
-public interface TrabajoRepository extends JpaRepository <Trabajo, Integer>{
-	
-	List<Trabajo> findAllByFechaInicio(LocalDate fechaInicio);
-
+public interface TrabajoRepository extends JpaRepository<Trabajo, Integer> {
+    
+    List<Trabajo> findAllByFechaInicio(LocalDate fechaInicio);
+    List<Trabajo> findAllByFechaEntregaBefore(LocalDate fechaHoy);
 }
+
