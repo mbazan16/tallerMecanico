57339ee494a70f15c10a78b70f316f7d43b439bb
Mañanas <Maanas@SB13-08>
2024-06-19 09:52:30 +0200

    Servicio reservas creados(inacabados)

---
+++ tallerMecanico/src/main/java/com/practica/tallerMecanico/services/reserva/IServicioReservas.java
@@ -0,0 +1,8 @@
+package com.practica.tallerMecanico.services.reserva;
+
+public interface IServicioReservas{
+	
+	public Reserva crearReserva(Reserva reserva);
+	
+	public void modificarReserva(Reserva reserva);	
+}
