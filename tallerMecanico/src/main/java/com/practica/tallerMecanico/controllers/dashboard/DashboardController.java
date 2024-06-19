bddfc92bec881a0daef30ddfbe562db49cd9661d
key <keylinjaimes@gmail.com>
2024-06-19 13:48:14 +0200

    actualizando servicios
    Merge branch 'master' of https://github.com/mbazan16/tallerMecanico.git

--- tallerMecanico/src/main/java/com/practica/tallerMecanico/controllers/dashboard/DashboardController.java
+++ tallerMecanico/src/main/java/com/practica/tallerMecanico/controllers/dashboard/DashboardController.java
@@ -1,8 +1,25 @@
 package com.practica.tallerMecanico.controllers.dashboard;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.practica.tallerMecanico.entities.Trabajo;
+import com.practica.tallerMecanico.services.dashboard.DashboardServicio;
 
 @Controller
 public class DashboardController {
 
-}
+	    @Autowired
+	    private DashboardServicio dashboardServicio;	    
+	    
+	    //Servicio trabajos para hoy
+	    @GetMapping("/pending")
+	    public List<Trabajo> getTrabajosHoy() {
+	        return dashboardServicio.getTrabajosHoy();
+	    }
+	}
+
+
