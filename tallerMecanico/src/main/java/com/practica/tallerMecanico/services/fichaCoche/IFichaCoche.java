6b387851793fee878cc9d44328b56b9feac27b9d
FrancoMañanas <Maanas@SB13-07>
2024-06-19 10:37:37 +0200

    Modificación FichaCocheSer

--- tallerMecanico/src/main/java/com/practica/tallerMecanico/services/fichaCoche/IFichaCoche.java
+++ tallerMecanico/src/main/java/com/practica/tallerMecanico/services/fichaCoche/IFichaCoche.java
@@ -3,6 +3,9 @@ package com.practica.tallerMecanico.services.fichaCoche;
 import java.util.List;
 import java.util.Optional;
 
+import com.practica.tallerMecanico.entities.Cliente;
+import com.practica.tallerMecanico.entities.Coche;
+
 public interface IFichaCoche {
 
 	public List<Coche> getAllCoches();
