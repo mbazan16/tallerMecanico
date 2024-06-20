683ff7db7d3e0b29c2fdfbaa2f5ade5ee1cd0ebc
mbazan16 <mariabazan@gmail.com>
2024-06-20 12:39:46 +0200

    Excepciones de servicio

---
+++ tallerMecanico/src/main/java/com/practica/tallerMecanico/services/common/ServiceException.java
@@ -0,0 +1,14 @@
+package com.practica.tallerMecanico.services.common;
+
+public class ServiceException extends Exception{
+	
+	private String code;
+
+	public ServiceException(String code) {
+		super();
+		this.code = code;
+	}
+	
+		
+
+}
