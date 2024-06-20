2e624e59dc15f74bf00130a2de0c5d5e2fb22344
mañanas <correofalso@falso.com>
2024-06-20 12:23:14 +0200

    Creado metodo getTrabajo y hecho el test

--- tallerMecanico/src/main/java/com/practica/tallerMecanico/controllers/entrega/EntregaController.java
+++ tallerMecanico/src/main/java/com/practica/tallerMecanico/controllers/entrega/EntregaController.java
@@ -1,5 +1,21 @@
 package com.practica.tallerMecanico.controllers.entrega;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.practica.tallerMecanico.services.entrega.IEntregaServicio;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@RequestMapping("/entrega")
+@Slf4j
 public class EntregaController {
+	
+	@Autowired
+	IEntregaServicio servicio;
+	
+	
 
 }
