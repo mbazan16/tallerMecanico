a6e689281f2fb4d32051dccca31b8f5195b321c0
mbazan16 <mariabazan@gmail.com>
2024-06-20 12:20:23 +0200

    Entidad Factura

---
+++ tallerMecanico/src/main/java/com/practica/tallerMecanico/entities/Factura.java
@@ -0,0 +1,14 @@
+package com.practica.tallerMecanico.entities;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Factura{
+	private long id;
+}
