f0ea5a874d62ff65bd602a34e858fc486ffff88d
FrancoMañanas <Maanas@SB13-07>
2024-06-20 13:57:41 +0200

    Ficha coche con primera excepcion y test completados

--- tallerMecanico/src/test/java/com/practica/tallerMecanico/services/fichaCoche/FichaCocheServicioTest.java
+++ tallerMecanico/src/test/java/com/practica/tallerMecanico/services/fichaCoche/FichaCocheServicioTest.java
@@ -1,6 +1,7 @@
 package com.practica.tallerMecanico.services.fichaCoche;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
 import java.util.Optional;
@@ -17,41 +18,53 @@ import org.slf4j.LoggerFactory;
 import com.practica.tallerMecanico.entities.Coche;
 import com.practica.tallerMecanico.repositories.ClienteRepository;
 import com.practica.tallerMecanico.repositories.CocheRepository;
+import com.practica.tallerMecanico.services.common.FichaCocheException;
 
 @ExtendWith(MockitoExtension.class)
 class FichaCocheServicioTest {
 
 	private static final Logger log = LoggerFactory.getLogger(FichaCocheServicioTest.class);
-	
-	 @Mock
-	    private CocheRepository cocheRepository;
-	  @Mock
-	    private ClienteRepository clienteRepository;
 
-	    @InjectMocks
-	    private FichaCocheServicio fichaCocheServicio;
+	@Mock
+	private CocheRepository cocheRepository;
+	@Mock
+	private ClienteRepository clienteRepository;
 
-	    private Coche coche;
+	@InjectMocks
+	private FichaCocheServicio fichaCocheServicio;
 
-	    @BeforeEach
-	    public void setUp() {
-	    	log.info("[Creación de instancia coche]");
-	        coche = new Coche();
-	        coche.setMatricula("AF2234");
-	        coche.setKilometraje(1999);
-	        coche.setMarca("Toyota");
-	        coche.setModelo("Yaris");
-	       
-	    }
-	 
+	private Coche coche;
+
+	@BeforeEach
+	public void setUp() {
+		log.info("[Creación de instancia coche]");
+		coche = new Coche();
+		coche.setMatricula("AF2234");
+		coche.setKilometraje(1999);
+		coche.setMarca("Toyota");
+		coche.setModelo("Yaris");
+
+	}
+
 	@Test
 	public void testGetCocheById() {
 		log.info("[testGetCocheById]");
 		when(cocheRepository.findById(1L)).thenReturn(Optional.of(coche));
-		
-		Optional<Coche> resultado = fichaCocheServicio.getCocheById(1L);
-		
-		assertEquals(Optional.of(coche), resultado);
+
+		Coche resultado = fichaCocheServicio.getCocheById(1L);
+
+		assertEquals(coche, resultado);
 	}
+	
+	 @Test
+	    public void testGetCocheById_NotFound() {
+	        when(cocheRepository.findById(1L)).thenReturn(Optional.empty());
+
+	        FichaCocheException exception = assertThrows(FichaCocheException.class, () -> {
+	            fichaCocheServicio.getCocheById(1L);
+	        });
+
+	        assertEquals("Coche no encontrado con ID: 1", exception.getMessage());
+	    }
 
 }
