cb593a84415e33d2423462952c289c02862089ca
Mañanas <Maanas@SB13-04.mshome.net>
2024-06-20 13:41:38 +0200

    Mapear la relacion cliente coche many to many

--- tallerMecanico/src/main/java/com/practica/tallerMecanico/entities/Cliente.java
+++ tallerMecanico/src/main/java/com/practica/tallerMecanico/entities/Cliente.java
@@ -1,7 +1,15 @@
 package com.practica.tallerMecanico.entities;
 
+import java.util.Set;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,8 +19,17 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Cliente{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	private String documento;
 	private String nombre;
 	private String telefono;
+	
+	@ManyToMany(fetch=FetchType.EAGER)
+	@JoinTable(name="Cliente_Coche",
+	joinColumns=@JoinColumn(name="idCliente"),
+	inverseJoinColumns=@JoinColumn(name="idCoche"))
+	private Set<Coche> coches;
+	
 }
