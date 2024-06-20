a6bf5f896e5f43ea202f9f93100be75fbc1301a6
Pedro <Maanas@SB13-08>
2024-06-20 13:54:31 +0200

    Creacion de excepcion de reserva y servicios de listado y busqueda

--- tallerMecanico/src/main/java/com/practica/tallerMecanico/services/reserva/ReservasServicio.java
+++ tallerMecanico/src/main/java/com/practica/tallerMecanico/services/reserva/ReservasServicio.java
@@ -8,6 +8,9 @@ import org.springframework.stereotype.Service;
 
 import com.practica.tallerMecanico.entities.Reserva;
 import com.practica.tallerMecanico.repositories.ReservaRepository;
+import com.practica.tallerMecanico.services.common.ErrorCode;
+import com.practica.tallerMecanico.services.common.MessageError;
+import com.practica.tallerMecanico.services.common.ReservaException;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -23,6 +26,7 @@ public class ReservasServicio implements IReservasServicio{
 		log.debug("[Reserva: "+reserva.toString()+"]");
 		try {
 			//inicializar el estado de la reserva que recibo
+			
 			reserva = repository.save(reserva);
 		} catch(Exception e) {
 			log.error("Error al crear reserva", e);
@@ -42,16 +46,42 @@ public class ReservasServicio implements IReservasServicio{
 		//modificar estado reserva a anulada y almacenarla?
 	}
 	
-	public List<Reserva> listarReservas(Reserva reserva) {
+	public List<Reserva> listarReservas(Reserva reserva) throws ReservaException {
 		log.info("[listarReserva]");
 		log.debug("[Reserva: "+reserva.toString()+"]");
-		
-		List<Reserva> reservas = repository.findAll();
+		List<Reserva> reservas = null;
+		try {
+			reservas = repository.findAll();
+			if(reservas.isEmpty()) throw new ReservaException(/*No es el correspondiente*/ErrorCode.EC_EXCEPCION_GENERAL);
+		} catch(ReservaException re) {
+			log.error(/*Mensaje sin elementos*/"Error sin elementos", re);
+			throw re;
+		} catch(Exception e) {
+			log.error(MessageError.EC_EXCEPCION_GENERAL, e);
+			throw new ReservaException(ErrorCode.EC_EXCEPCION_GENERAL);
+		}
 		return reservas;
 	}
 
-
-
-
+	public List<Reserva> buscar(String matricula, String telefono) throws ReservaException {
+		log.info("[buscar]");
+		log.debug("[buscar: "+matricula+telefono+"]");
+		List<Reserva> reservas = null;
+		try {
+			if(matricula==null || matricula.equals("")) {
+				//reservas = repository.findByMatricula();
+			}else {
+				//reservas = repository.findByTelefono();
+			}
+			if(reservas.isEmpty()) throw new ReservaException(/*Error de sin elementos*/ErrorCode.EC_EXCEPCION_GENERAL);
+		} catch(ReservaException re) {
+			log.error(/*mensaje sin elementos*/"Error sin elementos", re);
+			throw re;
+		} catch(Exception e) {
+			log.error(MessageError.EC_EXCEPCION_GENERAL, e);
+			throw new ReservaException(ErrorCode.EC_EXCEPCION_GENERAL);
+		}
+		return reservas;
+	}
 	
 }
