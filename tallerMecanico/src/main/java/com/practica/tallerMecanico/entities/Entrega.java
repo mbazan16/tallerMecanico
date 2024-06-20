e3b632c5b67a0fc115113c7fd37a6c2f7820812a
ripper21 <ripperlakus@gmail.com>
2024-06-20 10:57:27 +0200

    Configurar Nuevos campos a las entidades

--- tallerMecanico/src/main/java/com/practica/tallerMecanico/entities/Entrega.java
+++ tallerMecanico/src/main/java/com/practica/tallerMecanico/entities/Entrega.java
@@ -1,11 +1,13 @@
 package com.practica.tallerMecanico.entities;
 
-import jakarta.persistence.Column;
+import java.util.Date;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,5 +21,19 @@ public class Entrega {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date fechaEntrega;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date fechaRecogida;
+
+	private String tipo;
+
+	private Integer idCoche;
+
+	private Integer idCliente;
+
+	private Integer idTrabajo;
 
 }
--- tallerMecanico/src/main/java/com/practica/tallerMecanico/entities/Reserva.java
+++ tallerMecanico/src/main/java/com/practica/tallerMecanico/entities/Reserva.java
@@ -6,7 +6,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
 import lombok.AllArgsConstructor;
@@ -23,17 +22,21 @@ public class Reserva {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 
-	private String matricula;
-
-	private String cliente;
-
-	private String tipo;
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date fechaReserva;
 
 	@Temporal(TemporalType.TIMESTAMP)
-	private Date fechareserva;
+	private Date fechaEntrega;
 
-	private String prioridad;
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date fechaProgramacion;
 
-	private String observaciones;
+	private String Prioridad;
+
+	private Integer idCoche;
+
+	private Integer idCliente;
+
+	private Integer idTrabajo;
 
 }
