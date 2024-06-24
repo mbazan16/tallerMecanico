687469ba41c399d23eb0bb1beb61fe553290db9c
fazambrano24 <fazambrano24@gmail.com>
2024-06-24 13:24:53 +0200

    Querys nuevas

--- tallerMecanico/src/main/java/com/practica/tallerMecanico/repositories/TrabajoRepository.java
+++ tallerMecanico/src/main/java/com/practica/tallerMecanico/repositories/TrabajoRepository.java
@@ -26,7 +26,6 @@ public interface TrabajoRepository extends JpaRepository<Trabajo, Integer> {
     @Query("select t from Trabajo t where t.fechaProgramacion= :fechaInicio order by t.prioridad")
     List<Trabajo> findAllByFProgramacionAndEstado(EstadoTrabajo estado,LocalDate fechaProgramacion); //Busqueda por fecha de programacion listo
    
-    
     @Query("select t from Trabajo t where t.fechaProgramacion= :fechaInicio  order by t.prioridad")
     List<Trabajo> findAllByFProgramacion(LocalDate fechaProgramacion); //Busqueda por fecha de programacion listo
    
@@ -34,20 +33,28 @@ public interface TrabajoRepository extends JpaRepository<Trabajo, Integer> {
     List<Trabajo> findAllByFProgramacionAndMatricula(LocalDate fechaProgramacion,String matricula); //Busqueda por fecha de programacion listo
    
     @Query("select t from Trabajo t where t.fechaProgramacion= :fechaInicio and t.cliente.telefono= :telefono order by t.prioridad")
-    List<Trabajo> findAllByFProgramacionAndTelefono(LocalDate fechaProgramacion,String telefono); //Busqueda por fecha de programacion listo
-   
+    List<Trabajo> findAllByFProgramacionAndTelefono(LocalDate fechaProgramacion,String telefono); //Busqueda por fecha de programacion listo 
     
     //Ficha Coche
     @Query("select t from Trabajo t where t.coche.matricula= :matricula order by t.fechaInicio desc, t.prioridad")
     List<Trabajo> findAllByMatricula(String matricula); //Histórico por matrícula
     
-  //Ficha Coche
+    //Ficha Coche
     @Query("select t from Trabajo t where t.cliente.id= :id order by t.fechaInicio desc, t.prioridad")
     List<Trabajo> findAllByCliente(@Param("id")Long idCliente); //Trabajos por cliente
+
+    // Nuevas querys 24/06/2024
     
+  
+    @Query("SELECT t FROM Trabajo t WHERE t.cocheId = :cocheId AND t.fechaInicio = :fechaInicio")
+    List<Trabajo> findAllByCocheIdAndFechaInicio(@Param("cocheId") Long cocheId, @Param("fechaInicio") LocalDate fechaInicio); // Busqueda por fecha de inicio FechaInicio CocheId.FindAllByCocheIdAndFechaInicio 
+
+    @Query("SELECT t FROM Trabajo t WHERE t.coche.id = :cocheId")
+    List<Trabajo> findAllByCocheId(@Param("cocheId") Long cocheId); // CocheId findAllByCocheId
+    
+    @Query("SELECT t FROM Trabajo t WHERE t.coche.id = :cocheId AND t.coche.tipo = :tipo")
+    List<Trabajo> findAllByCocheIdAndCocheTipo(@Param("cocheId") Long cocheId, @Param("tipo") String tipo); // Tipo FindAllByCocheIDTipo
      
-    
-    
 }
 
 
