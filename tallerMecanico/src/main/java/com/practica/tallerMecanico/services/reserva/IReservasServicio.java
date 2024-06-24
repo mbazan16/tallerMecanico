709597d3f118c05655337f5493c4a4afaccd8909
Pedro <Maanas@SB13-08>
2024-06-24 13:45:54 +0200

    Metodos de servicio y metodos de controlador

--- tallerMecanico/src/main/java/com/practica/tallerMecanico/services/reserva/IReservasServicio.java
+++ tallerMecanico/src/main/java/com/practica/tallerMecanico/services/reserva/IReservasServicio.java
@@ -1,20 +1,21 @@
 package com.practica.tallerMecanico.services.reserva;
 
+import java.util.Date;
 import java.util.List;
 
+import com.practica.tallerMecanico.common.ReservaEstado;
 import com.practica.tallerMecanico.entities.Reserva;
-import com.practica.tallerMecanico.services.common.ReservaException;
 
 public interface IReservasServicio{
 	
 	public Reserva crearReserva(Reserva reserva);
 	
-	public void modificarReserva(Reserva reserva);
+	public Reserva modificarReserva(Reserva reserva);
 	
-	public void anularReserva(Reserva reserva);
+	public Reserva anularReserva(Reserva reserva);
 	
-	public List<Reserva> listarReservas(Reserva reserva) throws ReservaException;
+	public List<Reserva> listarReservas();
 	
-	public List<Reserva> buscar(String matricula, String telefono) throws ReservaException;
+	public List<Reserva> buscar(Date fecha, ReservaEstado estado, String matricula, String telefono);
 	
 }
