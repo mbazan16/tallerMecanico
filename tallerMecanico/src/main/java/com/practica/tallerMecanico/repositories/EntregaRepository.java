794d667cba86fa9b1e43526dc50bad07ce266964
ripper21 <ripperlakus@gmail.com>
2024-06-24 12:31:37 +0200

    Agregar nombre de los metodos a resevas

--- tallerMecanico/src/main/java/com/practica/tallerMecanico/repositories/EntregaRepository.java
+++ tallerMecanico/src/main/java/com/practica/tallerMecanico/repositories/EntregaRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.practica.tallerMecanico.entities.Reserva;
 
 public interface EntregaRepository extends JpaRepository <Reserva , Integer>{
+	
 
 }
