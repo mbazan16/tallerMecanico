ad4b3eeebd8aa4d78a89a3eb6192b1be2935c1a8
mbazan16 <mariabazan@gmail.com>
2024-06-25 12:04:43 +0200

    Se añade la búsqueda de coches por matricula

--- tallerMecanico/src/main/java/com/practica/tallerMecanico/repositories/CocheRepository.java
+++ tallerMecanico/src/main/java/com/practica/tallerMecanico/repositories/CocheRepository.java
@@ -1,9 +1,16 @@
 package com.practica.tallerMecanico.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.practica.tallerMecanico.entities.Coche;
 
 public interface CocheRepository extends JpaRepository<Coche, Long>{
 
+	
+	//Coche por matricula
+		@Query("SELECT c FROM Coche c WHERE c.matricula = :matricula")
+		Coche findByMatricula(String matricula);
+	
+		
 }
